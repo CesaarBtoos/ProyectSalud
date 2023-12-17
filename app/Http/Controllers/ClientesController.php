edfb6bcc4aa26efda6828793889150763b2e2baa
CesaarBtoos <cemartz120802@gmail.com>
2023-12-16 19:32:26 -0600

    actualizacion cliente

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -12,7 +12,7 @@ class ClientesController extends Controller
         return view('clientes.todas.index',['clientes' => Clientes::all()]);
     }
     
-    public function clientes3(){
+    public function clientes(){
 
         $clientes = Clientes::all();
         return view('nosotros',
