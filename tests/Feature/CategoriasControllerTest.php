7206c4a317a05e5356d73fd0d5f8a09cf9244cda
CesaarBtoos <cemartz120802@gmail.com>
2023-12-18 10:18:32 -0600

    prueba test 3

--- tests/Feature/CategoriasControllerTest.php
+++ tests/Feature/CategoriasControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -92,3 +92,5 @@ class CategoriasControllerTest extends TestCase
         $this->assertTrue(method_exists(CategoriasController::class, 'destrooy'));
     }
 }
+
+
